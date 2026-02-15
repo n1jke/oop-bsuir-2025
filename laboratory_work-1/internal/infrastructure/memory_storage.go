bd3fc17de9240213d334f1373e2e4e31ce8f3dbc
n1jke <pppprtoyy@gmail.com>
2026-02-15 16:50:44 +0300

    chore: adapt infrastructure to new domain model

--- laboratory_work-1/internal/infrastructure/memory_storage.go
+++ laboratory_work-1/internal/infrastructure/memory_storage.go
@@ -4,59 +4,58 @@ import (
 	"errors"
 
 	"github.com/google/uuid"
-	"github.com/n1jke/oop-bsuir-2025/lr-1/internal/application"
+
 	"github.com/n1jke/oop-bsuir-2025/lr-1/internal/domain"
 )
 
 var ErrNotFound = errors.New("not found")
 
 type MemoryAccountStorage struct {
-	byID map[uuid.UUID]application.PaymentAccount
+	byID map[uuid.UUID]*domain.Account
 }
 
 func NewMemoryAccountStorage() *MemoryAccountStorage {
-	return &MemoryAccountStorage{byID: make(map[uuid.UUID]application.PaymentAccount)}
+	return &MemoryAccountStorage{byID: make(map[uuid.UUID]*domain.Account)}
 }
 
-func (s *MemoryAccountStorage) Save(account application.PaymentAccount) error {
-	s.byID[account.AccountUUID()] = account
+func (s *MemoryAccountStorage) Save(account *domain.Account) error {
+	s.byID[account.ID()] = account
 	return nil
 }
 
-func (s *MemoryAccountStorage) ByUUID(accountUUID uuid.UUID) (application.PaymentAccount, error) {
-	account, exist := s.byID[accountUUID]
-	if !exist {
+func (s *MemoryAccountStorage) ByID(accountID uuid.UUID) (*domain.Account, error) {
+	account, exists := s.byID[accountID]
+	if !exists {
 		return nil, ErrNotFound
 	}
 
 	return account, nil
 }
 
-func (s *MemoryAccountStorage) UpdateStatus(accountUUID uuid.UUID, status domain.AccountStatus) error {
-	account, exist := s.byID[accountUUID]
-	if !exist {
+func (s *MemoryAccountStorage) UpdateStatus(accountID uuid.UUID, status domain.AccountStatus) error {
+	account, exists := s.byID[accountID]
+	if !exists {
 		return ErrNotFound
 	}
-	account.SetStatus(status)
-	s.byID[accountUUID] = account
+
+	account.ChangeStatus(status)
+
 	return nil
 }
 
 type MemoryEventStorage struct {
-	events []domain.Event
+	events []*domain.Event
 }
 
 func NewMemoryEventStorage() *MemoryEventStorage {
-	return &MemoryEventStorage{
-		events: make([]domain.Event, 0, 16),
-	}
+	return &MemoryEventStorage{events: make([]*domain.Event, 0, 16)}
 }
 
-func (s *MemoryEventStorage) Save(event domain.Event) error {
+func (s *MemoryEventStorage) Save(event *domain.Event) error {
 	s.events = append(s.events, event)
 	return nil
 }
 
-func (s *MemoryEventStorage) QueryAll() []domain.Event {
+func (s *MemoryEventStorage) QueryAll() []*domain.Event {
 	return s.events
 }
